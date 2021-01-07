78a68840f4d36d050bec03be6612b3ecf170bdf4
Kayro França <k4yro01@gmail.com>
2021-01-07 16:52:34 -0300

    mapping to the database

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -5,18 +5,35 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
     private Double latitude;
     private Double longitude;
     private Instant moment;
     private OrderStatus status;
-    private Double total;
 
+    @ManyToMany
+    @JoinTable(name = "tb_order_product",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
     private Set<Product> products = new HashSet<>();
 
     public Order() {
@@ -30,19 +47,10 @@ public class Order implements Serializable{
         this.setLongitude(longitude);
         this.setMoment(moment);
         this.setStatus(status);
-        this.setTotal(total);
     };
 
     public Set<Product> getProducts() {
         return products;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
     }
 
     public OrderStatus getStatus() {
