01010d218c907eff97a398667fa9a4c75d9a9fe3
Kayro França <k4yro01@gmail.com>
2021-01-08 01:20:10 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -39,8 +39,7 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
-            Double total) {
+    public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
         this.setId(id);
         this.setAddress(address);
         this.setLatitude(latitude);
