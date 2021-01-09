4c558bbb584e66b75dc4cff2b8b75fe530edcb75
Kayro França <k4yro01@gmail.com>
2021-01-09 12:09:24 -0300

    added NavBar

---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import { ReactComponent as Logo } from './logo.svg';
+
+
+const Navbar = () => {
+    return (
+        <nav className="main-navbar">
+            <Logo/>
+            <a href="home" className="logo-text">DS Deliver</a>
+        </nav>
+    );
+};
+
+
+
+export default Navbar;
